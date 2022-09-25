7e078f2ca533397ffe5bbe24a23aacafa25c9934
bryan <32003096+coolsitebiz@users.noreply.github.com>
2022-09-24 19:59:11 -0700

    Add operator function and helpers for each operation

--- script.js
+++ script.js
@@ -33,9 +33,38 @@ function updateInput(e) {
 }
 
 function clearScreen() {
-  console.log("clearing");
   input = 0;
   output = 0;
   screenInput.textContent = input;
   screenOutput.textContent = output;
+}
+
+function operate(operator, num1, num2) {
+  switch(operator) {
+    case("+"):
+      return add(num1, num2);
+    case("-"):
+      return subtract(num1, num2);
+    case("*"):
+      return multiplay(num1, num2);
+    case("/"):
+      return divide(num1, num2);
+  }
+
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplay(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
 }
