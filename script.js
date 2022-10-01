8ffb93455acc8bc6dfa7ab258a62de2d59ed3972
bryan <32003096+coolsitebiz@users.noreply.github.com>
2022-09-30 17:43:49 -0700

    Refactor several functions

--- script.js
+++ script.js
@@ -1,58 +1,50 @@
-let output = 0;
-let done = false;
-let num1 = 0;
-let num2 = 0;
-let operator = "";
-let curNum = num1;
+let displayNumber = 0;
+let term1 = null;
+let term2 = null;
+let operator = null;
+
+//need to set terms and fix calculate
+
 
 const screenOutput = document.querySelector("#screenContent");
-const key1 = document.querySelector("#one");
-const key2 = document.querySelector("#two");
-const key3 = document.querySelector("#three");
-const key4 = document.querySelector("#four");
-const key5 = document.querySelector("#five");
-const key6 = document.querySelector("#six");
-const key7 = document.querySelector("#seven");
-const key8 = document.querySelector("#eight");
-const key9 = document.querySelector("#nine");
-const keyClear = document.querySelector("#clear");
-const keyAdd = document.querySelector("#add");
-const keySubtract = document.querySelector("#subtract");
-const keyMultiply = document.querySelector("#multiply");
-const keyDivide = document.querySelector("#divide");
-const keyDecimal = document.querySelector("#decimal");
-const keyEquals = document.querySelector("#equals");
 
-//change to output update
-/* function updateInput(e) {
-  if (input === 0) {
-    input = e.dataset.value;
-    screenInput.textContent = input;
-  } else {
-    input += e.dataset.value;
-    screenInput.textContent = input;
-  }
-} */
+function negative() {
+  displayNumber = -displayNumber;
+  setDisplay(displayNumber);
+}
 
-function setNum(num) {
+function percent() {
+  displayNumber = displayNumber/100;
+  setDisplay(displayNumber);
+}
+
+function setOperator(op) {
+  operator = op.dataset.value;
+}
+
+function calculate() {
+  let result = operate(operator, term1, term2);
+  setDisplay(result);
+
+}
+
+function updateDisplay(num) {
   let value = num.dataset.value;
-  if (curNum === 0) {
-    if (value === "0") {
-      return;
-    }
-    curNum = value;
+  if (displayNumber === 0) {
+    displayNumber = value;
+    setDisplay(displayNumber);
   } else {
-    curNum += value;
+    displayNumber += value;
+    setDisplay(displayNumber);
   }
-  setDisplay(curNum);
 }
 
 function setDecimal() {
-  if (curNum.toString().includes(".")) {
+  if (displayNumber.toString().includes(".")) {
     return;
   }
-  curNum += ".";
-  setDisplay(curNum);
+  displayNumber += ".";
+  setDisplay(displayNumber);
 }
 
 function setDisplay(content) {
@@ -60,10 +52,10 @@ function setDisplay(content) {
 }
 
 function clearScreen() {
-  output = 0;
-  num1 = 0;
-  curNum = num1;
-  screenOutput.textContent = 0;
+  term1 = null;
+  term2 = null;
+  displayNumber = 0;
+  setDisplay(displayNumber);
 }
 
 function operate(operator, num1, num2) {
@@ -72,7 +64,7 @@ function operate(operator, num1, num2) {
       return add(num1, num2);
     case "-":
       return subtract(num1, num2);
-    case "*":
+    case "x":
       return multiply(num1, num2);
     case "/":
       return divide(num1, num2);
